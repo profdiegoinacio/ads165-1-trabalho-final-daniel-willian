a6cd66244cab945964e40780035adc5c15778eb7
DanielAlbq <175652@upf.br>
2025-04-23 12:48:20 -0300

    Integração com banco H2

--- backend/src/main/java/com/example/backend/Usuario.java
+++ backend/src/main/java/com/example/backend/Usuario.java
@@ -1,12 +1,24 @@
 package com.example.backend;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String endereco;
     private String email;
     private String telefone;
 
+    // Construtor vazio
+    public Usuario() {}
+
+    // Construtor
     public Usuario(Long id, String nome, String endereco, String email, String telefone) {
         this.id = id;
         this.nome = nome;
@@ -15,18 +27,43 @@ public class Usuario {
         this.telefone = telefone;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getEndereco() { return endereco; }
-    public void setEndereco(String endereco) { this.endereco = endereco; }
+    public String getNome() {
+        return nome;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-    public String getTelefone() { return telefone; }
-    public void setTelefone(String telefone) { this.telefone = telefone; }
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
--- backend/src/main/java/com/example/backend/controller/UsuarioController.java
+++ backend/src/main/java/com/example/backend/controller/UsuarioController.java
@@ -1,35 +1,27 @@
 package com.example.backend.controller;
 
 import com.example.backend.Usuario;
+import com.example.backend.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin(origins = "*")
 public class UsuarioController {
 
-    private List<Usuario> usuarios = new ArrayList<>();
-    private AtomicLong idCounter = new AtomicLong();
-
-    public UsuarioController() {
-        // Colocar os usuarios
-        usuarios.add(new Usuario(idCounter.incrementAndGet(), "Daniel Albuquerque", "Av Brasil, 111", "daniel@upf.br", "549830923092"));
-        usuarios.add(new Usuario(idCounter.incrementAndGet(), "Willian Calebe", "Av Brasil, 111", "willian@upf.br", "5498849384938"));
-    }
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @GetMapping
     public List<Usuario> listarUsuarios() {
-        return usuarios;
+        return usuarioRepository.findAll();
     }
 
     @PostMapping
     public Usuario cadastrarUsuario(@RequestBody Usuario usuario) {
-        usuario.setId(idCounter.incrementAndGet());
-        usuarios.add(usuario);
-        return usuario;
+        return usuarioRepository.save(usuario);
     }
 }
